e023db5909c83d0c504fd8499311af5003d130e3
Aliyugambo <gamboaliyu00@gmail.com>
2020-04-10 15:58:20 -0700

    iulding estimator

--- src/estimator.php
+++ src/estimator.php
@@ -1,20 +1,7 @@
 <?php
 
-function covid19ImpactEstimator($data,$region,$name,$avgAge,$avgDailyIncomeInUSD,$avgDailyPopulation,$periodType,$timeToElape,$reportedCases,$population,$totalHospitalBeds)
+function covid19ImpactEstimator($data)
 {
-  $data = '';
-  $region = '';
-  $name = "Africa";
-  $avgAge = "19.7";
-  $avgDailyIncomeInUSD = "5";
-  $avgDailyPopulation = " 0.71";
-  $periodType ="days";
-  $timeToElape = "58";
-  $reportedCases = "674";
-  $population  = "66622705";
-  $totalHospitalBeds = "1380614";
-
-  echo "$currentlyInfected = '$reportedCases * 10'";
+  return $data;
  
 }
-covid19ImpactEstimator("the currentlyInfected are:".$currentlyInfected);
