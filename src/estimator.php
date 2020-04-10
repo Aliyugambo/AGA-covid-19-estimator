e258079982fbacf141df0b2128334b2d519e0104
Aliyugambo <gamboaliyu00@gmail.com>
2020-04-10 14:33:41 -0700

    building estimator

--- src/estimator.php
+++ src/estimator.php
@@ -2,9 +2,5 @@
 
 function covid19ImpactEstimator($data)
 {
-  return ; {
-    data: {}, // the input data you got
-    impact: {}, // your best case estimation
-    severeImpact: {} // your severe case estimation
-    };
-};
+  return $data;
+}
