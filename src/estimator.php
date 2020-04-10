afb231a58770965afa6d7e6d9378f4584dffbad4
Aliyugambo <gamboaliyu00@gmail.com>
2020-04-10 15:20:43 -0700

    building estimator

--- src/estimator.php
+++ src/estimator.php
@@ -1,8 +1,19 @@
 <?php
 
-function covid19ImpactEstimator($data)
+function covid19ImpactEstimator($data,$region)
 {
-  return $data;
- $region = '';
- 
+
+  $region: {
+    $name: "Africa",
+    $avgAge: 19.7,
+    $avgDailyIncomeInUSD: 5,
+    $avgDailyIncomePopulation: 0.71
+    },
+    $periodType: "days",
+    $timeToElapse: 58,
+    $reportedCases: 674,
+   $population: 66622705,
+    $totalHospitalBeds: 1380614
+  return $data; 
+};
 }
