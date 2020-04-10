cd678e599593dee6bcffc0ea446e4ed805195bfc
Aliyugambo <gamboaliyu00@gmail.com>
2020-04-10 15:45:38 -0700

    biulding estimator

--- src/estimator.php
+++ src/estimator.php
@@ -1,8 +1,17 @@
 <?php
 
-function covid19ImpactEstimator($data)
+function covid19ImpactEstimator($data,$region,$name,$avgAge,$avgDailyIncomeInUSD,$avgDailyPopulation,$periodType,$timeToElape,$reportedCases,$population,$totalHospitalBeds)
 {
-  return $data;
- 
+  $data = '';
+  $region = '';
+  $name = "Africa";
+  $avgAge = "19.7";
+  $avgDailyIncomeInUSD = "5";
+  $avgDailyPopulation = " 0.71";
+  $periodType ="days";
+  $timeToElape = "58";
+  $reportedCases = "674";
+  $population  = "66622705";
+  $totalHospitalBeds = "1380614";
  
 }
