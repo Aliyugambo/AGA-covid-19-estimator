3858751064a17abe0be57264a60774e260b51418
Aliyugambo <gamboaliyu00@gmail.com>
2020-04-11 14:46:18 -0700

    buikding estimator

--- src/estimator.php
+++ src/estimator.php
@@ -2,24 +2,24 @@
 
 function covid19ImpactEstimator($data)
 {
-   data(){
-     $name = "Africa";
-     $avgAge = 19.7;
-     $avgDailyIncomeInUSD = 5; 
-     $avgDailyIncomePopulation = 0.71;
-   }
-   $periodType = "days";
-   $timeToElapse = 58;
-   $reportedCases = 674; 
-   $population = 66622705;
-   $totalHospitalBeds = 1380614;
+  //  data(){
+  //    $name = "Africa";
+  //    $avgAge = 19.7;
+  //    $avgDailyIncomeInUSD = 5; 
+  //    $avgDailyIncomePopulation = 0.71;
+  //  }
+  //  $periodType = "days";
+  //  $timeToElapse = 58;
+  //  $reportedCases = 674; 
+  //  $population = 66622705;
+  //  $totalHospitalBeds = 1380614;
    
-   impact($currentlyInfected){
-    // $currentlyInfected =($GLOBALS['$reportedCases']* 10);
-    // return$currentlyInfected;
-   }
-   severeImpact(){
+  //  impact($currentlyInfected){
+  //   // $currentlyInfected =($GLOBALS['$reportedCases']* 10);
+  //   // return$currentlyInfected;
+  //  }
+  //  severeImpact(){
 
-   }
+  //  }
    return$data;
 }
