5d53a3e9e15bff46120154e03b6fc66526eb89f9
Aliyugambo <gamboaliyu00@gmail.com>
2020-04-11 14:37:19 -0700

    building estimator

--- src/estimator.php
+++ src/estimator.php
@@ -2,21 +2,19 @@
 
 function covid19ImpactEstimator($data)
 {
+  global periodType = "days";
+  global timeToElapse = 58;
+  global reportedCases = 674; 
+  global population = 66622705;
+  global totalHospitalBeds = 1380614;
    data(){
      $name = "Africa";
      $avgAge = 19.7;
      $avgDailyIncomeInUSD = 5; 
      $avgDailyIncomePopulation = 0.71;
    }
-   $periodType = "days";
-   $timeToElapse = 58;
-   $reportedCases = 674; 
-   $population = 66622705;
-   $totalHospitalBeds = 1380614;
-   
    impact($currentlyInfected){
-    $currentlyInfected =($GLOBALS['$reportedCases']* 10);
-    return$currentlyInfected;
+    
    }
    severeImpact(){
 
